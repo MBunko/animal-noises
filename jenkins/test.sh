e61d0a22a637b3832734c3ad111664df27bcc143
htr-volker <htr.volker@gmail.com>
2021-01-21 10:18:04 +0000

    adding requests_mock pip install

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -8,7 +8,7 @@ cd service_1
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-pip3 install pytest pytest-cov flask_testing
+pip3 install pytest pytest-cov flask_testing requests_mock
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
 deactivate
 cd ..
@@ -18,7 +18,7 @@ cd service_2
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-pip3 install pytest pytest-cov flask_testing
+pip3 install pytest pytest-cov flask_testing requests_mock
 python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
 deactivate
 cd ..
