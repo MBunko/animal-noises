679642b376dbef478e2d1e2bba92c8820941da4e
htr-volker <htr.volker@gmail.com>
2021-01-21 19:36:37 +0000

    fixing deployment, setting app version on manager

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@swarm-master:/home/jenkins/docker-compose.yaml
 ssh -i ~/.ssh/ansible_id_rsa jenkins@swarm-master << EOF
-    export DATABASE_URI=${DATABASE_URI}
+    export DATABASE_URI=${DATABASE_URI} app_version=${app_version}
     docker stack deploy --compose-file /home/jenkins/docker-compose.yaml todo-stack
 EOF
