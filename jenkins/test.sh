493cde0e7597e2f5be9373c78f2a911f1c7de86a
htr-volker <htr.volker@gmail.com>
2021-01-21 09:52:11 +0000

    fixing test script

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -6,17 +6,19 @@ sudo apt-get install -y python3-venv python3-pip
 # Test service 1
 cd service_1
 python3 -m venv venv
+source venv/bin/activate
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov
-pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
+python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
 deactivate
 cd ..
 
 # Test service 2
 cd service_2
 python3 -m venv venv
+source venv/bin/activate
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov
-pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
+python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
 deactivate
 cd ..
