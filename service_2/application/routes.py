3ef06ed7c3033aa86fb2fe111aa9d1ab004f1ca7
Harry Volker <57865118+htr-volker@users.noreply.github.com>
2021-01-22 09:22:10 +0000

    Second Implementation (#3)
    * added chicken to potential animals
    
    * setting tag to v2
    
    * merge from main (#2)
    
    * fixing deployment config – replicas variable
    
    * Second Implementation (#1)
    
    * added chicken to potential animals
    
    * setting tag to v2
    
    * rollback to v1
    
    * Revert "fixing deployment config – replicas variable"
    
    This reverts commit 2bc633d1bba50b840dbf28ec090eaddb54b66315.
    
    * revert back to first implementation
    
    * revert back to first implementation (2)
    
    * re-adding replicas env var
    
    * fixing test for implementation 2
    
    * remove data.db creation for tests

--- service_2/application/routes.py
+++ service_2/application/routes.py
@@ -4,11 +4,11 @@ import random
 
 @app.route('/animal', methods=["GET"])
 def get_animal():
-    animals = ["cow", "pig", "horse"]
+    animals = ["cow", "pig", "horse", "chicken"]
     return Response(str(random.choice(animals)), mimetype='text/plain')
 
 @app.route('/noise', methods=["POST"])
 def get_noise():
-    noises = {"cow":"moo", "pig":"oink", "horse":"neigh"}
+    noises = {"cow":"moo", "pig":"oink", "horse":"neigh", "chicken":"cluck"}
     animal = request.json
     return Response(noises[animal["animal"]], mimetype='text/plain')
